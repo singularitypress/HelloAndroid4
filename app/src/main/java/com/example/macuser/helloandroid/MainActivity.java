5bf7e608b51f75ee188d63ca54243764549a719e
singularitypress <jpandya1@sy-s2112-33.local>
2016-01-20 21:50:36 -0500

    adding app

---
+++ app/src/main/java/com/example/macuser/helloandroid/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.macuser.helloandroid;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.util.*;
+
+
+//public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
+
+    //TAG == APPNAME
+    private static String TAG = "This is a TAG";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i(TAG, " jay p 123 456 7890");
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
